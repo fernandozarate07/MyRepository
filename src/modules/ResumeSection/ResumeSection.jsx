668c3ebdbb6183cc05a1d9e3ca37f9ad6bf84861
Fernando Zárate <150300719+fernandozarate07@users.noreply.github.com>
2025-01-26 15:54:42 -0300

    Refactor aboutme section

--- src/modules/ResumeSection/ResumeSection.jsx
+++ src/modules/ResumeSection/ResumeSection.jsx
@@ -2,6 +2,7 @@ import { useRef } from "react";
 import html2canvas from "html2canvas";
 import jsPDF from "jspdf";
 import styles from "./ResumeSection.module.css";
+import logo from "../../assets/image/logo.svg";
 
 const ResumeSection = () => {
   const targetRef = useRef();
@@ -32,20 +33,47 @@ const ResumeSection = () => {
   };
 
   return (
-    <div className={styles.resume}>
-      <button onClick={handleDownloadPDF} className={styles.resume__downloadBtn}>
-        DESCARGAR CV
-      </button>
-      <div className={styles.printContainer}>
-        <div ref={targetRef} className={styles.resume__container}>
-          {/* portada */}
-          <div className={styles.resume__portada}>
-            <div className={styles.resume_nameContainer}>
-              <h1 className={styles.resume__name}>Fernando Zárate</h1>
-              <p className={styles.resume__description}>Desarrollador Web</p>
-            </div>
-            <div className={styles.resume__contact}>
-              <i className="fa-solid fa-house"></i>
+    <div className={styles.aboutme}>
+      <div className={styles.aboutme__descriptionContainer}>
+        <h2 className={styles.aboutme__title}>Sobre Mí</h2>
+        <p className={styles.aboutme__descriptionInfo}>
+          <span>
+            <i className="fa-solid fa-check"></i>
+            Manejo un stack basado en tecnologías modernas como JavaScript, React, Next.js, Node.js, Express y
+            PostgreSQL.
+          </span>
+          <span>
+            <i className="fa-solid fa-check"></i>
+            cuento con experiencia en WordPress, lo que me permite ofrecer soluciones personalizadas cuando es
+            necesario.
+          </span>
+          <span>
+            <i className="fa-solid fa-check"></i>
+            Comencé mi formación como programador de manera autodidacta en 2023, y hoy en día me encuentro cursando la
+            Tecnicatura en Programación Web en la Universidad Nacional de San Juan.
+          </span>
+          <span>
+            <i className="fa-solid fa-check"></i>
+            ¿Les cuento algo sobre mí? Soy músico y docente. Me han dicho que soy una persona creativa. Algo de verdad
+            debe haber en eso, porque encuentro en los procesos creativos todo lo que me apasiona: desde componer una
+            canción hasta desarrollar una aplicación.
+          </span>
+        </p>
+        <button onClick={handleDownloadPDF} className={styles.aboutme__downloadBtn}>
+          DESCARGAR CV
+        </button>
+      </div>
+      <div className={styles.aboutme__resumeContainer}>
+        <div ref={targetRef} className={styles.aboutme__resumeContent}>
+          {/*portada */}
+          <div className={styles.aboutme__resumePortada}>
+            <h3 className={styles.aboutme__resumeName}>Fernando Zárate</h3>
+            <p className={styles.aboutme__resumeDesciption}>Desarrollador Fullstack</p>
+          </div>
+          {/* contact*/}
+          <div className={styles.aboutme__resumeContact}>
+            <div className={styles.aboutme__resumeInfo}>
+              <img src={logo} alt="logo" />
               <span>www.fernandozarate.website</span>
               <i className="fa-solid fa-mobile-screen-button"></i>
               <span>+54 264 6271151</span>
@@ -56,90 +84,7 @@ const ResumeSection = () => {
               <i className="fa-solid fa-location-dot"></i>
               <span>San Juan, Argentina </span>
             </div>
-          </div>
-          {/* sobre mí */}
-          <div className={`${styles.resume__containerAboutme} ${styles.section}`}>
-            <h2 className={styles.resume__subtitle}>SOBRE MÍ</h2>
-            <p className={styles.resume__aboutmeDescription}>
-              <span>
-                Soy un profesional creativo con una profunda pasión por el diseño. Mi formación como programador comenzó
-                de manera autodidacta en 2023, en 2025 decidí formalizar mis conocimientos, comenzando mis estudios en
-                programación web en la Universidad Nacional de San Juan.
-              </span>
-              <span>
-                Mis estudios previos en música me permiten combinar mis habilidades técnicas en programación con una
-                visión artística y funcional de los proyectos. Como profesor de música, he desarrollado una sólida
-                formación pedagógica que me ha permitido adquirir habilidades transversales clave, como la comunicación
-                efectiva, el aprendizaje autónomo, la resolución de problemas y el trabajo en equipo. Mi capacidad para
-                abordar desafíos de manera innovadora y mi enfoque en los procesos creativos son la base de mi
-                desarrollo profesional y personal.
-              </span>
-            </p>
-          </div>
-          {/* experiencia */}
-          <div className={`${styles.resume__containerExperience} ${styles.section}`}>
-            <h2 className={styles.resume__subtitle}>EXPERIENCIA LABORAL</h2>
-            <div className={styles.resume__sectionExperience}>
-              <h3>Experiencia como Programador</h3>
-              <p>
-                Soy fundador de una agencia de desarrollo web, Protoprogramadores, donde me desempeño como desarrollador
-                frontend y especialista en WordPress. Mi portafolio web (enlace disponible en la sección de contacto)
-                reúne proyectos que demuestran mi capacidad para aplicar conocimientos de programación web en contextos
-                prácticos, así como mi compromiso con el aprendizaje continuo y la excelencia profesional.
-              </p>
-            </div>
-            <div className={styles.resume__sectionExperience}>
-              <h3>Otras Experiencias</h3>
-              <p>
-                He trabajado en diversos puestos, siendo el último como docente de grado en una escuela primaria en San
-                Juan, Argentina. Ejercí la titularidad del cargo desde 2022 hasta mediados de 2024, donde adquirí una
-                valiosa experiencia en la gestión de aulas, el desarrollo de planes de enseñanza y la formación de
-                estudiantes, lo que fortaleció mis habilidades de comunicación, liderazgo y resolución de problemas.
-              </p>
-            </div>
-          </div>
-          {/* educación y stack */}
-          <div className={`${styles.resume__containerEducationStack} ${styles.section}`}>
-            {/* educación */}
-            <div className={styles.resume__containerEducation}>
-              <h2 className={styles.resume__subtitle}>EDUCACIÓN</h2>
-              <div className={styles.resume__contentEstudies}>
-                <div className={styles.resume__educationItem}>
-                  <span className={styles.resume__educationDate}>2025 - PRESENTE</span>
-                  <div className={styles.containerEducationSpan}>
-                    <span>Tecnicatura en Programación Web</span>
-                    <span>Universidad Nacional de San Juan</span>
-                  </div>
-                </div>
-                <div className={styles.resume__educationItem}>
-                  <span className={styles.resume__educationDate}>2016 - 2021</span>
-                  <div className={styles.containerEducationSpan}>
-                    <span>Profesora en Educación Musical</span>
-                    <span>Instituto superior Santa Cecilia</span>
-                  </div>
-                </div>
-              </div>
-            </div>
-            {/* stack */}
-            <div className={styles.resume__containerStack}>
-              <h2 className={styles.resume__subtitle}>TECNOLOGÍAS</h2>
-              <div className={styles.resume__contentStack}>
-                <div className={styles.resume__stackContainer}>
-                  <span> • HTML</span>
-                  <span> • CSS</span>
-                  <span> • Bootstrap</span>
-                  <span> • Sass</span>
-                  <span> • WordPress</span>
-                </div>
-                <div className={styles.resume__stackContainer}>
-                  <span> • JavaScript</span>
-                  <span> • React</span>
-                  <span> • Git</span>
-                  <span> • GitHub</span>
-                  <span> • WooCommerce</span>
-                </div>
-              </div>
-            </div>
+            <div className={styles.aboutme__resumeQrContainer}></div>
           </div>
         </div>
       </div>
