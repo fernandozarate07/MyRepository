80b7d0490039c3aa1a8ab032ee20d7d4bee6f58b
Fernando Zárate <150300719+fernandozarate07@users.noreply.github.com>
2025-01-27 01:56:02 -0300

    Add logo in home section

--- src/modules/HomeSection/HomeSection.jsx
+++ src/modules/HomeSection/HomeSection.jsx
@@ -1,11 +1,15 @@
 import Primarybutton from "../Primarybutton/Primarybutton.jsx";
 import styles from "./HomeSection.module.css";
 import Calltoaction from "../Callaction/Cta.jsx";
+import logo from "../../assets/image/logo.svg";
 
 const HomeSection = () => {
   return (
     <div className={styles.homeSection}>
-      <Calltoaction link={"https://www.linkedin.com/in/fernandozaratedev/"} content={"Disponible para trabajar"} />
+      <div className={styles.homeSection__logo}>
+        <img src={logo} alt="logo" className={styles.logo} width={100} />
+        <Calltoaction link={"https://www.linkedin.com/in/fernandozaratedev/"} content={"Disponible para trabajar"} />
+      </div>
       <div className={styles.homeSection__content}>
         <div className={styles.homeSection__containerDescription}>
           <h1 className={styles.homeSection__title}>Hola, Soy Fernando.</h1>
@@ -14,11 +18,11 @@ const HomeSection = () => {
           </h2>
           <p className={styles.homeSection__para}>¿Qué te trae por aquí?</p>
         </div>
-        <div className={styles.homeSection__ctaContainer}>
-          <Primarybutton section={"projects"} content=" VER PROYECTOS" />
-          <Primarybutton section={"aboutme"} content=" DESGARGAR CV" />
-          <Primarybutton section={"contactme"} content=" CONTACTARME" />
-        </div>
+      </div>
+      <div className={styles.homeSection__ctaContainer}>
+        <Primarybutton section={"projects"} content=" VER PROYECTOS" />
+        <Primarybutton section={"aboutme"} content=" DESGARGAR CV" />
+        <Primarybutton section={"contactme"} content=" CONTACTARME" />
       </div>
     </div>
   );
